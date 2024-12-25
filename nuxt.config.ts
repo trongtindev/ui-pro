23a57aabddef8431c7c075fa7ee472a08994a45a
Tin Nguyen Trong <me@trongtin.dev>
2024-12-25 10:36:58 +0700

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,14 @@
-import { createResolver } from '@nuxt/kit'
+import { createResolver } from "@nuxt/kit";
 
-const { resolve } = createResolver(import.meta.url)
+const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
-  components: [
-    { path: '#ui-pro/components', prefix: 'U', pathPrefix: false }
-  ],
-  css: [
-    '#ui-pro/assets/css/main.css'
-  ],
-  alias: { '#ui-pro': resolve('./') },
+  components: [{ path: "#ui-pro/components", prefix: "U", pathPrefix: false }],
+  css: ["#ui-pro/assets/css/main.css"],
+  alias: { "#ui-pro": resolve("./") },
   vite: {
     optimizeDeps: {
-      include: ['vue3-smooth-dnd']
-    }
-  }
-})
+      include: ["vue3-smooth-dnd"],
+    },
+  },
+});
